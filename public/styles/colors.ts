2fe5318bed0ba07f8f0afa4b4bd9f4d9e4bfbbcb
Lucas Lima Torquato <lucaslimatorquato@hotmail.com>
2023-01-10 16:56:36 -0300

    Segunda Tarefa

---
+++ public/styles/colors.ts
@@ -0,0 +1,19 @@
+interface ColorsProps {
+    green: string;
+    darkGreen: string;
+    pink: string;
+    darkPink: string;
+    white: string;
+    black: string;
+    gray: string;
+  }
+  
+  export const colors: ColorsProps = {
+    green: '#A0A77C',
+    white: '#FFFFFF',
+    black: '#4E545C',
+    darkGreen: '#5C735E',
+    darkPink: '#CC8D80',
+    pink: '#E5BFB7',
+    gray: '4E545C',
+  };
